1e011802046de964dbaa59980dd629fc08012d2c
Felix Jacobi <Felix@jacobi-hamburg.net>
2021-07-09 02:47:35 +0200

    Prepare to allow SSH only from IServ GmbH, private networks and Stadtteilschule Blankenese local network

---
+++ iconf/etc/ferm.d/20iserv.conf/90stsbl-backend-server.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Restrict SSH on hosts under stsbl.de to local network except for
+# router.stsbl.de which acts as safe habour for broken VPN for now
+if false && [ "$(hostname -d)" = "stsbl.de" ] && ! [ "$(hostname)" = "router.stsbl.de" ]
+then
+  cat <<EOT
+# Return before server-ferm SSH accept
+domain (ip ip6) table filter chain input_world proto tcp dport ssh RETURN;
+
+EOT
+fi
+
+exit 0
